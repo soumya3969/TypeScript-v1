27a63bf533d4ddc512e4e932e8cf41de8fa5a6eb
soumya3969 <srmhnt2001@gmail.com>
2025-01-10 15:38:52 +0530

    installation

---
+++ 01.Intro/intro.ts
@@ -0,0 +1,8 @@
+let user = {name : "Soumya Ranjan Mahanta", age : 25};
+
+console.log(user.name)
+console.log(user.email)
+
+
+console.log("Soumya Ranjan Mahanta");
+
---
+++ Day-1/one.ts
@@ -0,0 +1,3 @@
+let users = {name: 'John', age: 25};
+// !let users2 = users.email; // Error: Property 'email' does not exist on type '{ name: string; age: number; }'.
+// but we can do it in javascript
